6e2f810bcddf7af29c26c9e68c0f463f993d0ee5
Efrem-Yohanis <efremyohanis116@gmail.com>
2023-06-03 19:58:54 +0300

    'Test-Suit-and-Test-Case'

--- cypress/e2e/firest_cy_code.cy.js
+++ cypress/e2e/firest_cy_code.cy.js
@@ -1,5 +1,22 @@
-describe('Test Suit',()=>{
-    it('Test Case',()=>{
+describe('Test Suit1',()=>{
+    it('Test Case1',()=>{
+       
       cy.log("this is arrow function")
     })
+
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit2',function (){
+    it('Test Case1', ()=>{
+       
+      cy.log("this is arrow function")
+    })
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
 });
