e179aae07239545a24f1b8c61d57a81da45a6191
Efrem-Yohanis <efremyohanis116@gmail.com>
2023-06-03 20:14:53 +0300

    'Test-Suit-and-Test-Case'

--- cypress/e2e/firest_cy_code.cy.js
+++ cypress/e2e/firest_cy_code.cy.js
@@ -20,3 +20,95 @@ describe('Test Suit2',function (){
         cy.log("this is nameless function")
       })
 });
+
+describe('Test Suit3',()=>{
+    it('Test Case1',()=>{
+       
+      cy.log("this is arrow function")
+    })
+
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit4',function (){
+    it('Test Case1', ()=>{
+       
+      cy.log("this is arrow function")
+    })
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit5',()=>{
+    it('Test Case1',()=>{
+       
+      cy.log("this is arrow function")
+    })
+
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit6',function (){
+    it('Test Case1', ()=>{
+       
+      cy.log("this is arrow function")
+    })
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit7',()=>{
+    it('Test Case1',()=>{
+       
+      cy.log("this is arrow function")
+    })
+
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit8',function (){
+    it('Test Case1', ()=>{
+       
+      cy.log("this is arrow function")
+    })
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit9',()=>{
+    it('Test Case1',()=>{
+       
+      cy.log("this is arrow function")
+    })
+
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
+
+describe('Test Suit10',function (){
+    it('Test Case1', ()=>{
+       
+      cy.log("this is arrow function")
+    })
+    it('Test Case2',function (){
+       
+        cy.log("this is nameless function")
+      })
+});
