a17f9b6bf0267f70d3b66b928529844f2d7aa721
Efrem-Yohanis <efremyohanis116@gmail.com>
2023-06-06 06:16:20 +0300

    visit_command

--- cypress/e2e/visit_command.cy.js
+++ cypress/e2e/visit_command.cy.js
@@ -1,6 +1,33 @@
 describe('visit_command',()=>{
-    it('visit google',()=>{
-        //test step
-        cy.visit('http://www.google.com')
-    });
+     // 1. open google 
+     it.skip('visit google',()=>{
+     //test step
+     cy.visit('http://www.google.com')
+     });
+    
+      // 2. open Craft websit 
+     it.skip('visit craft websit',()=>{
+         cy.visit('https://craftknowledge.net/')
+     });
+
+     // 3. open local project
+     it.skip('visit craft websit',()=>{
+         cy.visit('http://127.0.0.1:5500/project/home.html')
+     });
+
+    //4.  baseUrl concept
+
+    it.skip('home ',()=>{
+         cy.visit('home');
+        
+     });
+
+   //5. Changing BaseUrl While Testing
+    it('Changing BaseUrl While Testing',{baseUrl: 'http://127.0.0.1:5500/project/'},()=>{
+         
+            cy.visit('home.html')
+        });
+    
+
+    
 })
